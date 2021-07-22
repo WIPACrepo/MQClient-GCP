0df18e41fdc9fc157711efb6d5e93785190893f3
Ric Evans <emejqz@gmail.com>
2021-07-22 13:42:59 -0500

    use `MQClient@v0.0.8`; mypy fix

--- integrate_tests/test_gcp.py
+++ integrate_tests/test_gcp.py
@@ -1,20 +1,24 @@
 """Run integration tests for GCP backend."""
 
-# local imports
-import mqclient.testing_frameworks.integration_tests as it
-from mqclient.testing_frameworks.integration_tests.utils import (  # pytest.fixture # noqa: F401 # pylint: disable=W0611
+import logging
+
+from mqclient.testing import integrate_backend_interface, integrate_queue
+from mqclient.testing.utils import (  # pytest.fixture # noqa: F401 # pylint: disable=W0611
     queue_name,
 )
 from mqclient_gcp.gcp import Backend
 
+logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger("flake8").setLevel(logging.WARNING)
 
-class TestGCPQueue(it.common_queue_tests.PubSubQueue):
+
+class TestGCPQueue(integrate_queue.PubSubQueue):
     """Run PubSubQueue integration tests with GCP backend."""
 
     backend = Backend()
 
 
-class TestGCPBackend(it.common_backend_interface_tests.PubSubBackendInterface):
+class TestGCPBackend(integrate_backend_interface.PubSubBackendInterface):
     """Run PubSubBackendInterface integration tests with GCP backend."""
 
     backend = Backend()
