d237f7f527ccebf484b7271392d2b2f54d001bbf
Ric Evans <emejqz@gmail.com>
2021-11-08 17:48:32 -0600

    add wipactel stdout

--- examples/run-with-wipactel-local.sh
+++ examples/run-with-wipactel-local.sh
@@ -5,6 +5,7 @@ if [[ `basename "$PWD"` != "MQClient-GCP" ]] ; then
 	exit 1
 fi
 
+export WIPACTEL_EXPORT_STDOUT=${WIPACTEL_EXPORT_STDOUT:="TRUE"}
 export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318/v1/traces"
 export WIPACTEL_SERVICE_NAME_PREFIX=mqclient-gcp
 
