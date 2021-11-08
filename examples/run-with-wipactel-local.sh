e24d05252a4dfd51e4f94481c449b9c8a0a41a39
Ric Evans <emejqz@gmail.com>
2021-11-08 17:39:58 -0600

    add pwd guards

--- examples/run-with-wipactel-local.sh
+++ examples/run-with-wipactel-local.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [[ `basename "$PWD"` != "MQClient-GCP" ]] ; then
+	echo "ERROR: Run from 'MQClient-GCP/'"
+	exit 1
+fi
+
 export OTEL_EXPORTER_OTLP_ENDPOINT="http://localhost:4318/v1/traces"
+export WIPACTEL_SERVICE_NAME_PREFIX=mqclient-gcp
 
 pip install tox
 tox --notest -vv
--- examples/run.sh
+++ examples/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ `basename "$PWD"` != "MQClient-GCP" ]] ; then
+	echo "ERROR: Run from 'MQClient-GCP/'"
+	exit 1
+fi
+
 gcloud beta emulators pubsub start --project="i3-gcp-proj" &
 export PUBSUB_EMULATOR_HOST=localhost:8085
 sleep 2
