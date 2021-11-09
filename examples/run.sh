46bbabccf67074dd869f750416d567c11b994102
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2021-11-08 18:17:55 -0600

    Tracing and Req Updates (#7)

--- examples/run.sh
+++ examples/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ `basename "$PWD"` != "MQClient-GCP" && $PWD != "/home/circleci/project" ]] ; then
+	echo "ERROR: Run from 'MQClient-GCP/' (not '$PWD')"
+	exit 1
+fi
+
 gcloud beta emulators pubsub start --project="i3-gcp-proj" &
 export PUBSUB_EMULATOR_HOST=localhost:8085
 sleep 2
